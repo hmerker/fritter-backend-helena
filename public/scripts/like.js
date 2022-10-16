606a52f845cf6cc6e7ceb4dac195d12ecbf6a4c3
Helena <hmerker@mit.edu>
2022-10-16 19:46:16 -0400

    changes

--- public/scripts/like.js
+++ public/scripts/like.js
@@ -1,12 +1,18 @@
 
 function viewLikeByItem(fields) {
-    fetch(`/api/likes?itemId=${fields.itemId}`).then(showResponse).catch(showResponse);
-  }
-  
-  function createLike(fields) {
-    fetch("/api/likes", {method: "POST", body: JSON.stringify(fields), headers: {"Content-Type": "application/json"},}).then(showResponse).catch(showResponse);
-  }
-  
-  function deleteLike(fields) {
-    fetch(`/api/likes/${fields.itemId}`, {method: "DELETE"}).then(showResponse).catch(showResponse);
-  }
+  fetch(`/api/likes?itemId=${fields.itemId}`)
+    .then(showResponse)
+    .catch(showResponse);
+}
+
+function createLike(fields) {
+  fetch('/api/likes', {method: 'POST', body: JSON.stringify(fields), headers: {'Content-Type': 'application/json'}})
+    .then(showResponse)
+    .catch(showResponse);
+}
+
+function deleteLike(fields) {
+  fetch(`/api/likes/${fields.itemId}`, {method: 'DELETE'})
+    .then(showResponse)
+    .catch(showResponse);
+}
