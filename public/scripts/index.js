0d5e20378a4340c242f8300b3be977e902cd0090
Helena <hmerker@mit.edu>
2022-10-16 19:25:37 -0400

    worked more on like

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -38,7 +38,10 @@ const formsAndHandlers = {
   'view-freets-by-author': viewFreetsByAuthor,
   'create-freet': createFreet,
   'edit-freet': editFreet,
-  'delete-freet': deleteFreet
+  'delete-freet': deleteFreet,
+  'create-like': createLike,
+  'delete-like': deleteLike,
+  'view-like': viewLikeByItem
 };
 
 // Attach handlers to forms
